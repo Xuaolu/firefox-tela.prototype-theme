d2bc295088e9fbb6d231de86f74611caece5031f
João Lucas <joaolucas.pacc@gmail.com>
2024-08-04 22:40:26 -0300

    upload

---
+++ configuration/user.js
@@ -0,0 +1,21 @@
+/* user.js
+ * https://github.com/rafaelmardojai/firefox-gnome-theme/
+ */
+
+// Enable customChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Set UI density to normal
+user_pref("browser.uidensity", 0);
+
+// Enable SVG context-propertes
+user_pref("svg.context-properties.content.enabled", true);
+
+// Allows colours used in the theme to be mixed with others.
+user_pref("layout.css.color-mix.enabled", true);
+
+// Allows theme to use different colours for light/dark mode.
+user_pref("layout.css.light-dark.enabled", true);
+
+// Enables the CSS :has() selector, required for hide tabs toolbar tweak.
+user_pref("layout.css.has-selector.enabled", true);
