ad48bde3f4290d4735e08556f9a4d54569582df1
Xuaolu <119270979+Xuaolu@users.noreply.github.com>
2024-08-23 17:37:03 -0300

    Update user.js

--- configuration/user.js
+++ configuration/user.js
@@ -8,14 +8,14 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // Set UI density to normal
 user_pref("browser.uidensity", 0);
 
-// Enable SVG context-propertes
+// Enable SVG context-properties
 user_pref("svg.context-properties.content.enabled", true);
 
 // Allows colours used in the theme to be mixed with others.
-user_pref("layout.css.color-mix.enabled", true);
+//user_pref("layout.css.color-mix.enabled", true);
 
 // Allows theme to use different colours for light/dark mode.
-user_pref("layout.css.light-dark.enabled", true);
+//user_pref("layout.css.light-dark.enabled", true);
 
 // Enables the CSS :has() selector, required for hide tabs toolbar tweak.
-user_pref("layout.css.has-selector.enabled", true);
+//user_pref("layout.css.has-selector.enabled", true);
